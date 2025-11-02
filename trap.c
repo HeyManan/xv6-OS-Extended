8ab7eb421ac84a9d7e43fcf28328639446946bdf
HeyManan <masumpatel111.mp@gmail.com>
2025-11-02 14:52:08 -0500

    Broke page allocation and added lazy page allocation

--- trap.c
+++ trap.c
@@ -14,6 +14,50 @@ extern uint vectors[];  // in vectors.S: array of 256 entry pointers
 struct spinlock tickslock;
 uint ticks;
 
+static pte_t *
+walkpgdir(pde_t *pgdir, const void *va, int alloc)
+{
+  pde_t *pde;
+  pte_t *pgtab;
+
+  pde = &pgdir[PDX(va)];
+  if(*pde & PTE_P){
+    pgtab = (pte_t*)P2V(PTE_ADDR(*pde));
+  } else {
+    if(!alloc || (pgtab = (pte_t*)kalloc()) == 0)
+      return 0;
+    // Make sure all those PTE_P bits are zero.
+    memset(pgtab, 0, PGSIZE);
+    // The permissions here are overly generous, but they can
+    // be further restricted by the permissions in the page table
+    // entries, if necessary.
+    *pde = V2P(pgtab) | PTE_P | PTE_W | PTE_U;
+  }
+  return &pgtab[PTX(va)];
+}
+
+static int
+mappages(pde_t *pgdir, void *va, uint size, uint pa, int perm)
+{
+  char *a, *last;
+  pte_t *pte;
+
+  a = (char*)PGROUNDDOWN((uint)va);
+  last = (char*)PGROUNDDOWN(((uint)va) + size - 1);
+  for(;;){
+    if((pte = walkpgdir(pgdir, a, 1)) == 0)
+      return -1;
+    if(*pte & PTE_P)
+      panic("remap");
+    *pte = pa | perm | PTE_P;
+    if(a == last)
+      break;
+    a += PGSIZE;
+    pa += PGSIZE;
+  }
+  return 0;
+}
+
 void
 tvinit(void)
 {
@@ -80,6 +124,33 @@ trap(struct trapframe *tf)
             cpuid(), tf->cs, tf->eip);
     lapiceoi();
     break;
+  case T_PGFLT:
+      uint va;
+      char *mem;
+
+      va = rcr2();
+      if (va < KERNBASE && va < myproc()->sz) {
+        mem = kalloc();
+        if (mem == 0) {
+          cprintf("lazy alloc: kalloc failed for pid %d\n", myproc()->pid);
+        }
+        else {
+          memset(mem, 0, PGSIZE);
+
+          if (mappages(myproc()->pgdir, (void*)PGROUNDDOWN(va), PGSIZE, V2P(mem), PTE_W|PTE_U) < 0) {
+            cprintf("lazy alloc: mapping failed for pid %d ", myproc()->pid);
+            kfree(mem);
+          }
+          else break;
+        }
+      }
+
+      cprintf("pid %d %s: trap %d err %d on cpu %d "
+            "eip 0x%x addr 0x%x--kill proc\n",
+            myproc()->pid, myproc()->name, tf->trapno,
+            tf->err, cpuid(), tf->eip, rcr2());
+      myproc()->killed = 1;
+      break;
 
   //PAGEBREAK: 13
   default:
